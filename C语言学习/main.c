8b83409aaed0f13ccfec135bd370df22e47fd1fb
xdl <xudl@huayunsoft.com>
2024-01-10 20:19:25 +0800

    Initial Commit

---
+++ C语言学习/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  C语言学习
+//
+//  Created by xdl on 2024/1/10.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
